4a0dd4078f1db03930156fb3159e73ed113051c0
Just-Vs <99630937+Just-Vs@users.noreply.github.com>
2022-07-28 09:18:12 +0100

    Player test

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
+
 const Score = require('../models/score')
 router.get('/', (req, res)=> {
   res.render('index')
