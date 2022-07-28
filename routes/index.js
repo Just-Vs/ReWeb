f61ceaa64a5fd7c0dd5a3e46dcb6d655627cba2e
Just-Vs <99630937+Just-Vs@users.noreply.github.com>
2022-07-28 16:34:15 +0100

    Score Test 3

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,12 @@ const Score = require('../models/score')
 
 router.get('/', async (req, res)=> {
   let searchOptions = {}
-  if (req.query.name != null && req.query.name !==''){
-    searchOptions.name = new RegExp(req.query.name, 'i')
-  }
   try{
-    const scores = await Score.find(searchOptions)
-    res.render('score/index', {scores: scores, searchOptions: req.query})
+    const scores = await Score.find({})
+    res.render('index', {scores: scores})
   }catch{
-
+    
   }
-  res.render('index')
 })
 
 module.exports = router
